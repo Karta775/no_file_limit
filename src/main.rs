bb2bf2223be71c25e9abe47ce47125d796d8e17f
Karta775 <Karta775@users.noreply.github.com>
2022-07-20 09:37:53 +0100

    Add an interactive selection if the user didn't specify a chunk size
    The interactive selection is achieved via the dialoguer crate and allows users to select between Discord, Whatsapp, FAT, and a custom input.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use helper::*;
 use clap::Parser;
 use std::path::PathBuf;
 use std::io;
+use dialoguer::{theme::ColorfulTheme, Select, Input};
+
+const SELECTION_DISCORD: &str = "Discord";
+const SELECTION_WHATSAPP: &str = "Whatsapp";
+const SELECTION_FS_FAT: &str = "FAT filesystem";
+const SELECTION_CUSTOM: &str = "Custom chunk size";
 
 /// Defeat awful file size limits
 #[derive(Parser, Debug)]
@@ -15,27 +21,63 @@ struct Args {
     filepath: String,
 
     /// The chunk size in megabytes
-    #[clap(short, long, value_parser, default_value="8")]
-    chunk_size: usize,
+    #[clap(short, long, value_parser)]
+    chunk_size: Option<usize>,
 }
 
 fn main() -> Result<(), io::Error>{
     let args = Args::parse();
     let path = PathBuf::from(&args.filepath);
+    // TODO: Error handling - file not found / no extension
     let extension = path.extension().unwrap().to_str().unwrap().to_owned();
 
+    // Set the chunk size in MiB from commandline arguments or interactive mode
+    let chunk_size = match args.chunk_size {
+        Some(size) => size,
+        None => select_chunk_size()
+    };
+
+    // If the metadata file is found then reconstruct, otherwise deconstruct
     if extension == METADATA_FILE_EXTENSION {
         let glue = Glue::new(&args.filepath);
-        println!("Started reconstructing the file");
-        glue.reconstruct()
+        glue.reconstruct() // TODO: Error handling - don't panic
             .expect("Something went wrong while trying to reconstruct the file");
-
     } else if extension != METADATA_FILE_EXTENSION {
-        let slicer = Slicer::new(&args.filepath, args.chunk_size);
-        println!("Started deconstructing the file");
-        slicer.deconstruct()
+        let slicer = Slicer::new(&args.filepath, chunk_size as usize);
+        slicer.deconstruct() // TODO: Error handling - don't panic
             .expect("Something went wrong while trying to deconstruct the file");
     }
 
     return Ok(())
+}
+
+fn select_chunk_size() -> usize {
+    let selections = &[
+        SELECTION_DISCORD,
+        SELECTION_WHATSAPP,
+        SELECTION_FS_FAT,
+        SELECTION_CUSTOM,
+    ];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Where are you sending the file?")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    let chunk_size: usize = match selections[selection] {
+        SELECTION_DISCORD => 8,
+        SELECTION_WHATSAPP => 100,
+        SELECTION_FS_FAT => 4192,
+        SELECTION_CUSTOM => { // TODO: Error handling - bad input
+            let num: String = Input::new()
+                .with_prompt("Enter the size in megabytes")
+                .interact_text().expect("Something went wrong");
+            num.parse().unwrap()
+        }
+        _ => 0,
+    };
+
+    chunk_size
 }
